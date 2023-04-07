4ba9f5afcdda6b2674e5fbf23c03cf9daf193359
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-07 13:50:34 +0530

    Notes on Memory Stack, Memory heap, Frame, Memory method, Pass by value

--- src/com/nitrousoxide/Main.java
+++ src/com/nitrousoxide/Main.java
@@ -2,6 +2,9 @@ package com.nitrousoxide;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("abc" + 9);
+
     }
 }

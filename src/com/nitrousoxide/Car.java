b9a729b7e0006751f620a7617eb738cfee213f44
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-22 20:46:39 +0530

    Polymorphism, and Interface

---
+++ src/com/nitrousoxide/Car.java
@@ -0,0 +1,18 @@
+package com.nitrousoxide;
+
+public class Car implements Vehical{
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void applyBrakes() {
+
+    }
+
+    @Override
+    public void getCurrentSpeed() {
+
+    }
+}
---
+++ src/com/nitrousoxide/Vehical.java
@@ -0,0 +1,8 @@
+package com.nitrousoxide;
+
+public interface Vehical {
+
+    public void move();
+    public void applyBrakes();
+    public void getCurrentSpeed();
+}

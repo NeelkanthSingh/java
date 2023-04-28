c98989fcf1236541a7e1e53b7f10b9fed13762e5
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-28 20:37:47 +0530

    Added Bounded class

--- src/com/nitrousoxide/GenericAndMethods.java
+++ src/com/nitrousoxide/GenericAndMethods.java
@@ -6,6 +6,11 @@ public class GenericAndMethods {
         String[] stringArray = {"a", "b", "c"};
         print(intArray);
         print(stringArray);
+
+        Integer[] intArray2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(print(intArray2, 3));
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5};
+        System.out.println(print(doubleArray, 3.3));
     }
 
     static <T> void print(T[] array) {
@@ -15,6 +20,16 @@ public class GenericAndMethods {
                             " - " + e
             );
         }
+    }
+
+    static <T extends Comparable<T>> int print(T[] t, T n){
+        int count=0;
+        for (T e: t) {
+            if(e.compareTo(n)>0){
+                count++;
+            }
+        }
+        return count;
     }
 
 }

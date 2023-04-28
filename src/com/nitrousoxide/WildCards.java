ed275493f5fa0f5c64eae1571940b14c99163dd3
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-28 20:52:45 +0530

    Upper bounded Wildcards bit more restrictive

--- src/com/nitrousoxide/WildCards.java
+++ src/com/nitrousoxide/WildCards.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class WildCards {
     public static void main(String[] args) {
         Integer[] ints = {1, 2, 3};
-        String[] strings = {"a", "b", "c"};
+        Double[] doubles = {1.1, 2.2, 3.3};
         print(List.of(ints));
-        print(List.of(strings));
+        print(List.of(doubles));
     }
 
-    static void print(List<?> list) {
+    static void print(List<? extends Number> list) {
         list.forEach(System.out::println);
     }
 }

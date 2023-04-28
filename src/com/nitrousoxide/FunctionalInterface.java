918283f798824b1ef2fb703e1b7caeb142980003
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-29 02:23:43 +0530

    Functional Interface and Other important terms

--- src/com/nitrousoxide/FunctionalInterface.java
+++ src/com/nitrousoxide/FunctionalInterface.java
@@ -3,19 +3,47 @@ package com.nitrousoxide;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3);
 
-        // Predicate is a functional interface
-        Predicate<Integer> isEven = e -> e % 2 == 0;
+        // Predicate
+        Predicate<Integer> isOdd = e -> e % 2 != 0;
+        Predicate<Integer> isMultipleOfThree = e -> e % 3 == 0;
+        // Function
+        Function<Integer,Integer> incrementByOneFunction = e -> e + 1;
+        Function<Integer,Integer> multiplyByTenFunction = e -> e * 10;
+        Function<Integer,Integer> incrementByOneAndMultiplyByTenFunction = incrementByOneFunction.andThen(multiplyByTenFunction);
+
+        // BiFunction
+        BiFunction<Integer,Integer,Integer> incrementByOneAndMultiplyByTenBiFunction = (e, n) -> (e + 1) * n;
+
+        // Consumer
+        Consumer<Integer> printAge = age -> System.out.println(age);
+
+        // BiConsumer
+        BiConsumer<Integer, String> printAgeAndName = (age, name) -> System.out.println(age + " " + name);
+
+        // Supplier
+        Supplier<List<Integer>> listSupplier = () -> new ArrayList<>(Arrays.asList(1, 2, 3));
 
         list.
                 stream().
-                filter(isEven).
+                filter(isOdd).
                 forEach(System.out::println);
+
+        listSupplier.get().forEach(System.out::println);
+        System.out.println(isOdd.test(7));
+        System.out.println(isOdd.and(isMultipleOfThree).test(7));
+        System.out.println(isOdd.or(isMultipleOfThree).test(7));
+        System.out.println(incrementByOneFunction.apply(7));
+        System.out.println(incrementByOneAndMultiplyByTenFunction.apply(7));
+        System.out.println(incrementByOneAndMultiplyByTenBiFunction.apply(7, 100));
+        printAge.accept(7);
+        printAgeAndName.accept(7, "John Doe");
+
 
     }
 }

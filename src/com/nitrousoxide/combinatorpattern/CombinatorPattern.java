0e9a51c818dd0a2d09a4d709d181823f5fa8591f
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-29 18:38:24 +0530

    Callback

--- src/com/nitrousoxide/combinatorpattern/CombinatorPattern.java
+++ src/com/nitrousoxide/combinatorpattern/CombinatorPattern.java
@@ -1,6 +1,8 @@
 package com.nitrousoxide.combinatorpattern;
 import com.nitrousoxide.combinatorpattern.CustomRegistrationValidator.ValidationResult;
 
+import java.util.function.Consumer;
+
 import static com.nitrousoxide.combinatorpattern.CustomRegistrationValidator.ValidationResult.SUCCESS;
 import static com.nitrousoxide.combinatorpattern.CustomRegistrationValidator.isAnAdult;
 import static com.nitrousoxide.combinatorpattern.CustomRegistrationValidator.isPhoneNumberValid;;
@@ -24,5 +26,18 @@ public class CombinatorPattern {
             throw new IllegalStateException(result.name());
         }
         else System.out.println("Validation passed.");
+
+        hello("John", "Mathew", value -> {
+            System.out.println("No last name for " + value);
+        });
+    }
+
+    static void hello(String firstName, String lastName, Consumer<String> callback){
+        System.out.println(firstName);
+        if(lastName!=null){
+            System.out.println(lastName);
+        }else{
+            callback.accept(firstName);
+        }
     }
 }
